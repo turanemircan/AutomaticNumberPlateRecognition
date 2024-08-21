c464bf17e026e056faf9da919f10a723a2c95202
Emircan <emircanturan010@gmail.com>
2024-08-21 13:23:01 +0300

    RandomForestModel oluşturuldu

--- CharacterRecognition.py
+++ CharacterRecognition.py
@@ -7,34 +7,69 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import os
 
+
 path = "Character Set/"
 classes = os.listdir(path)
 singleBatch = 0
 
-urls= [] # Karakter resimlerinin adreslerini tutacak
-grades = []
-
-for grade in classes:
-    images = os.listdir(path+grade)
+urls = []
+classLabels = []
+for cls in classes:
+    images = os.listdir(path + cls)
     for image in images:
-        urls.append(path+grade+"/"+image)
-        grades.append(grade)
+        urls.append(path + cls + "/" + image)
+        classLabels.append(cls)
         singleBatch += 1
 
-dataFrame = pd.DataFrame({"URL":urls,"Grade":grades})
+df = pd.DataFrame({"address": urls, "class": classLabels})
 
 def process(img):
-    newHeight = img.reshape((1600,5,5))
-    averages = []
-    for piece in newHeight:
-        average = np.mean(piece)
-        averages.append(average)
-    averages = np.array(averages)
-    averages = averages.reshape(1600,)
-    return averages
     
-def pretreatment(img):
-    return img/255
+    newShape = img.reshape((1600, 5, 5))
+    means = []
+    for part in newShape:
+        mean = np.mean(part)
+        means.append(mean)
+    means = np.array(means)
+    means = means.reshape(1600,)
+    return means
 
-targetSize = (200,200)
-batchSize = singleBatch
+def preprocess(img):
+    return img / 255
+
+target_size = (200, 200) 
+batch_size = singleBatch 
+
+train_gen = tf.keras.preprocessing.image.ImageDataGenerator(
+    preprocessing_function=preprocess)
+
+train_set = train_gen.flow_from_dataframe(df, x_col="address", y_col="class",
+                                          target_size=target_size,
+                                          color_mode="grayscale",
+                                          shuffle=True,
+                                          class_mode='sparse',
+                                          batch_size=batch_size)
+
+images, train_y = next(train_set)
+train_x = np.array(list(map(process, images))).astype("float32")
+train_y = train_y.astype(int)
+
+print("Random Forest / Rassal Orman Modeli Eğitiliyor...")
+
+randomForestClass = RandomForestClassifier(n_estimators=10,criterion="entropy")
+
+randomForestClass.fit(train_x,train_y)
+
+print("Random Forest / Rassal Orman Modeli Eğitildi.")
+
+pred = randomForestClass.predict(train_x)
+
+acc = accuracy_score(pred,train_y)
+
+print("Random Forest / Rassal Orman Modeli Doğruluk Oranı: ",acc)
+
+file = "RandomForestModel.rfc"
+
+pickle.dump(randomForestClass,open(file, "wb")) # Modeli şifreli kaydetme işlemi
+
+# dosyayı okumak için pickle.load(open(file, "rb")) kullanılır
